9bb9707198d23b2919055e5fb0257ad8127caf38
kthemendoza <kthemendoza@gmail.com>
2019-04-07 12:37:43 -0500

    se puso contenido a la alerta inventario

--- src/app/inventario/inventario.page.ts
+++ src/app/inventario/inventario.page.ts
@@ -9,9 +9,95 @@ import { AlertController } from '@ionic/angular';
 })
 export class InventarioPage implements OnInit {
 
-  constructor() { }
-
+  constructor(
+    public  alert: AlertController,
+  ) { }
+  goToSignup(){
+    this.showAlert()
+  }
+  // goToSignup1(){
+  //   this.showAlert("Success!", "Welcome aboard!")
+  // }
+  // goToSignup2(){
+  //   this.showAlert("Success!", "Welcome aboard!")
+  // }
+  // goToSignup3(){
+  //   this.showAlert("Success!", "Welcome aboard!")
+  // }
+  // goToSignup4(){
+  //   this.showAlert("Success!", "Welcome aboard!")
+  // }
+   
+   
   ngOnInit() {
   }
+  async showAlert() {
+    const alert = await this.alert.create({
+      header: 'Checkbox',
+      inputs: [
+        {
+          name: 'checkbox1',
+          type: 'checkbox',
+          label: 'Checkbox 1',
+          value: 'value1',
+          checked: true
+        },
+
+        {
+          name: 'checkbox2',
+          type: 'checkbox',
+          label: 'Checkbox 2',
+          value: 'value2'
+        },
+
+        {
+          name: 'checkbox3',
+          type: 'checkbox',
+          label: 'Checkbox 3',
+          value: 'value3'
+        },
+
+        {
+          name: 'checkbox4',
+          type: 'checkbox',
+          label: 'Checkbox 4',
+          value: 'value4'
+        },
+
+        {
+          name: 'checkbox5',
+          type: 'checkbox',
+          label: 'Checkbox 5',
+          value: 'value5'
+        },
+
+        {
+          name: 'checkbox6',
+          type: 'checkbox',
+          label: 'Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6',
+          value: 'value6'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  } 
 
 }
+
+
